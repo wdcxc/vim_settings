fc673a054ec9de5154c9b9ceedabbd2c8f050a7a
wdcxc <904342189@qq.com>
2016-08-07 23:01:26 +0800

    modify:setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mv .vimrc .vimrc.vundle ~\
-mkdir -p ~/.vim/bundle\
-cp -r colors ~/.vim/\
-cd .vim/bundle && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim\
+mv .vimrc .vimrc.vundle ~
+mkdir -p ~/.vim/bundle
+cp -r colors ~/.vim/
+cd .vim/bundle && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-
+cd -
